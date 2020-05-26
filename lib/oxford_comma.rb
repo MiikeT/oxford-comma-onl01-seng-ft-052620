6f670ecda653c1ab68aeb13ec1f00acc472216fe
Michael Takeuchi <miketaki@sbcglobal.net>
2020-05-26 20:50:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-  array.join(" and ")
+  if array.length <= 2
+    array.join(" and ")
+  elsif array.length == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    popped = array.pop
+    array.join(", ") + popped
 end
