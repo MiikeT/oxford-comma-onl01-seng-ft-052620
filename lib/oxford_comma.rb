eea336cca377963de5f8db2b97ca7a4437775c48
Michael Takeuchi <miketaki@sbcglobal.net>
2020-05-26 20:51:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,6 @@
 def oxford_comma(array)
   if array.length <= 2
     array.join(" and ")
-  elsif array.length == 3
-    popped = array.pop
-    "#{array.join(", ")}, and #{popped}"
   else
     popped = array.pop
     "#{array.join(", ")}, and #{popped}"
